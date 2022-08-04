284b199713e6db78ed2535c79560dd74e29be05f
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-08-04 20:50:36 +0100

    <<<<<<<

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./navbar.css";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse mr-5" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item pr-2 pl-5 active">
             <a className="nav-link  a1" href="#">
