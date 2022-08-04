25e18b1bcd68454a585568baab5d44199ec823fb
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-08-04 15:49:38 +0100

    <<<<<<

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+import image from "../images/img.jpg";
+export default function Header() {
+  return (
+    <div className="container ">
+      <img className=" round-image" src={image} alt="." />
+    </div>
+  );
+}
