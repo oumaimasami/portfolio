5e0d53f51abb88db2c7f1cc7054c66dcc5683f0c
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-08-04 21:07:25 +0100

    wwwww

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,23 +7,23 @@ export default function Navbar() {
       <div className="collapse navbar-collapse mr-5" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item pr-2 pl-5 active">
-            <a className="nav-link  a1" href="#">
-              Home <span className="sr-only">(current)</span>
+            <a className="nav-link  a1" href="#About">
+              About <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item pr-2">
-            <a className="nav-link a2" href="#About">
-              About
+            <a className="nav-link a2" href="#Projects">
+              Projects
             </a>
           </li>
           <li className="nav-item pr-2">
-            <a className="nav-link  a3" href="#Products">
-              Products
+            <a className="nav-link  a3" href="#Cantact">
+              Cantact
             </a>
           </li>
           <li className="nav-item pr-2">
-            <a className="nav-link  a4" href="#Cantact">
-              Contact Us
+            <a className="nav-link  a4" href="#HireMe">
+              Hire Me !
             </a>
           </li>
         </ul>
