1fc20d4c12eebaff686d54cd7f43dd8332e2d193
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-08-04 18:08:02 +0100

    <<<<<<

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./header.css";
+import Typed from "react-typed";
 import image from "../images/img.jpg";
 export default function Header() {
   return (
-    <div className="container ">
+    <div className="container row center">
       <img className="round-image" src={image} alt="." />
+      <Typed
+        className="col-12 typed-text"
+        strings={[
+          "I'm a Business Intelligence Student",
+          "I'm a Full Stack Developer",
+        ]}
+        typeSpeed={40}
+        backSpeed={50}
+        loop
+      />
     </div>
   );
 }
