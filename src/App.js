4ef6dc3a5adc262cfb69904ce7e76cbca72e2506
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-08-05 15:53:54 +0100

    <<<<<<

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About";
+import Cantact from "./components/Cantact";
 import Header from "./components/Header";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <About />
+      <Cantact />
     </>
   );
 }
