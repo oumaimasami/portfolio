a6344f5b1ef0601b5ad9ac7b2f135d2687fdd0e3
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-08-07 20:43:57 +0100

    <<<<<<<

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -2,20 +2,17 @@ import React from "react";
 import "./hireme.css";
 export default function HireMe() {
   return (
-    <div className="container d-flex hire" id="HireMe">
-      <form className="justify-content-center text-center">
+    <div className="container hire mt-5" id="HireMe">
+      <form className=" text-center">
         <div className="row form-group mt-5 mb-3">
-          <label className="mx-1" for="exampleFormControlInput1 pt-2">
-            Email
-          </label>
           <input
             type="email"
-            className="form-control"
+            className="form-control mt-3"
             id="exampleFormControlInput1"
             placeholder="name@example.com"
           />
         </div>
-        <div className="btn HireButton">Hire me !</div>
+        <div className="btn HireButton py-3">Hire me !</div>
       </form>
     </div>
   );
