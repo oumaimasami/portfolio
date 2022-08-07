290fd7a3038c59b2d798de51368a05a0559b471c
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-08-07 21:16:06 +0100

    <<<<<<<<

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./hireme.css";
+import image from "../images/1.png";
 export default function HireMe() {
   return (
     <div className="container hire mt-5" id="HireMe">
-      <form className=" text-center">
-        <div className="row form-group mt-5 mb-3">
-          <input
-            type="email"
-            className="form-control mt-3"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-          />
+      <div className="row">
+        <div className="col-4 ">
+          <img className="hire-image" src={image} alt="." />
         </div>
-        <div className="btn HireButton py-3">Hire me !</div>
-      </form>
+        <form className=" col-8 text-center">
+          <div className="row form-group mt-5 pb-4">
+            <input
+              type="email"
+              className="form-control mt-3"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="btn HireButton py-2">Hire me !</div>
+        </form>
+      </div>
     </div>
   );
 }
