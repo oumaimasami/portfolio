900a9123ea497361f076b876e5c657ba6fd96f68
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2023-02-26 22:45:24 +0100

    <<<<<<

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./hireme.css";
+import emailjs from "@emailjs/browser";
 import image from "../images/1.png";
 import image1 from "../images/2.png";
 export default function HireMe() {
   const [clicked, setclicked] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_r72yvus",
+        "template_0xt807n",
+        form.current,
+        "2BCE8pznkONmSs6JL"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="container hire">
       <div className="row mt-3 pt-3 justify-content-center">
@@ -14,18 +36,27 @@ export default function HireMe() {
             <img className="hire-image" src={image} alt="." />
           )}
         </div>
-        <form className=" col-lg-8 col-md-12 col-sm-12 text-center">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className=" col-lg-8 col-md-12 col-sm-12 text-center"
+        >
           <div className="row pl-4 form-group mt-5 pt-5 pb-4">
             <input
               type="email"
               className="form-control col-lg-11 col-md-12 col-sm-12  mt-3"
               id="exampleFormControlInput1"
               placeholder="name@example.com"
+              name="email"
             />
           </div>
-          <div className="btn HireButton py-2" onClick={() => setclicked(true)}>
+          <button
+            className="btn HireButton py-2"
+            onClick={() => setclicked(true)}
+            type="submit"
+          >
             Hire me !
-          </div>
+          </button>
         </form>
       </div>
     </div>
